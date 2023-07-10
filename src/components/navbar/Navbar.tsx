ea7078b4b6ed9edef31b1e3b77c7e53fd8c67531
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-10 14:27:57 +0200

    Created Navbar for portfolio

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import styles from "./navbar.module.sass";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.contentBox}>
+        <h1>Bochen</h1>
+        <ul className={styles.anchors}>
+          <li>
+            <a>
+              <h2>About me</h2>
+            </a>
+          </li>
+          <li>
+            <a>
+              <h2>Skills</h2>
+            </a>
+          </li>
+          <li>
+            <a>
+              <h2>Projects</h2>
+            </a>
+          </li>
+          <li>
+            <a className={styles.contact}>
+              <h2>Contact</h2>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
