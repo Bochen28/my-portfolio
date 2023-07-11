466b8af34de81314f7bff36fbacb3f7d70ae7ee2
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-11 14:31:23 +0200

    Added git anchor to intro section

--- src/components/sections/intro/Intro.tsx
+++ src/components/sections/intro/Intro.tsx
@@ -2,18 +2,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import styles from "./intro.module.sass";
 
+const gitICon = <FontAwesomeIcon icon={faGithub} className={styles.icon} />;
+
 function Intro() {
   return (
     <section className={styles.intro}>
       <div className={styles.introText}>
         <h1>
-          Hi, I am <span>Patryk.</span>
+          Hi, I am <span>Patryk.</span><br />
+          Your Future Frontend Developer
         </h1>
         <a className={styles.resume}>
           <h3>My Resume</h3>
         </a>
-        <a className={styles.git}>
-          <FontAwesomeIcon icon={faGithub} style={{ color: "#4169e1" }} />
+        <a className={styles.gitBtn} href="https://github.com/Bochen28" target="_blank">
+          {gitICon}
         </a>
       </div>
     </section>
