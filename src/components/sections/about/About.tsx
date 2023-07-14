a3e75bb7858c953331ebf0bbb12bcc5cc3c01a7a
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-14 03:21:49 +0200

    Added about me text

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -6,7 +6,19 @@ function About() {
     <>
       <section id="about" className={styles.about}>
         <div className={styles.content}>
-            
+          <h1>Time for some bragging.</h1>
+          <h2>
+            I'm a <span>passionate and dedicated</span> junior frontend
+            developer with a<span> strong enthusiasm</span> for creating
+            engaging and user-friendly web experiences.{" "}
+            <span>Adaptability</span> is one of my strengths. I embrace new
+            technologies and trends in the ever-evolving world of frontend
+            development,{" "}
+            <span>constantly seeking opportunities to expand my knowledge</span>
+            . Feel free to reach out to me for any opportunities,
+            collaborations, or simply to connect. Let's bring ideas to life and
+            make the web a better place!
+          </h2>
         </div>
         <TopWave />
       </section>
