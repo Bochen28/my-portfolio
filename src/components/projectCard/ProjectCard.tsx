778152000b7189a7ccefa82fb523a862a25350e5
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-20 23:43:57 +0200

    Added project card animation

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -7,7 +7,8 @@ type ProjectProps = {
 function ProjectCard({name}: ProjectProps) {
     return(
         <div className={styles.card}>
-            <h2>{name}</h2>
+            <h2 className={styles.front}>{name}</h2>
+            <h2 className={styles.back}>sample</h2>
         </div>
     )
 }
