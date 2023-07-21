933ea3bbaca2e2296bc21693271e238e7b23f671
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-21 14:38:35 +0200

    fixed ProjectCard display and added git and live button

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -1,16 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSatelliteDish } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import styles from "./projectCard.module.sass";
 
 type ProjectProps = {
-    name: string,
-}
+  name: string;
+  gitLink: string;
+  liveLink: string;
+};
 
-function ProjectCard({name}: ProjectProps) {
-    return(
-        <div className={styles.card}>
-            <h2 className={styles.front}>{name}</h2>
-            <h2 className={styles.back}>sample</h2>
-        </div>
-    )
+function ProjectCard({ name, gitLink, liveLink }: ProjectProps) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.front}>
+        <h2>{name}</h2>
+      </div>
+      <div className={styles.back}>
+        <h2>sample</h2>
+        <a href={gitLink}><FontAwesomeIcon icon={faGithub} /></a>
+        <a href={liveLink}><FontAwesomeIcon icon={faSatelliteDish} /></a>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
