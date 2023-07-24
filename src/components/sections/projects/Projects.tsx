08f6145c1749be64848dab38d190fcecb80ca950
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-24 14:42:07 +0200

    Changed colors for the project

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -2,13 +2,14 @@ import ProjectCard from "@/components/projectCard/ProjectCard";
 import styles from "./projects.module.sass";
 
 function Projects() {
-    return (
-        <section id="projects" className={styles.projects}>
-            <div className={styles.content}>
-                <ProjectCard name="Sample Text" />
-            </div>
-        </section>
-    )
+  return (
+    <section id="projects" className={styles.projects}>
+      <div className={styles.content}>
+        <h2 className={styles.heading}>My Projects:</h2>
+        <ProjectCard name="Sample Text" gitLink="" liveLink="" />
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
