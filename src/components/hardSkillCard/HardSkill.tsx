526415fe915cdd77314261b711ae0bcf5b260d0c
Patryk Bochenek <patryk.bochenek28@gmail.com>
2023-07-27 21:54:51 +0200

    Created dropdown list of what's next

--- src/components/hardSkillCard/HardSkill.tsx
+++ src/components/hardSkillCard/HardSkill.tsx
@@ -1,20 +1,40 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./hardSkill.module.sass";
 
 function HardSkill() {
+  const [isVisible, setVisible] = useState(true);
+
+  const toggleDropdown = () => {
+    setVisible(!isVisible);
+  };
+
   return (
     <div className={styles.content}>
       <h2>Hard Skills</h2>
-      <div className={styles.card}>
-        <ul>
-            <li>Html</li>
-            <li>Css</li>
-            <li>Sass</li>
-            <li>Js</li>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>NextJs</li>
-            <li>BootStrap</li>
+      <div className={styles.card} onClick={toggleDropdown}>
+        <ul className={styles.skillsList}>
+          <li>Html</li>
+          <li>Css</li>
+          <li>Sass</li>
+          <li>Js</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>BootStrap</li>
         </ul>
+        <div
+          className={styles.dropdown}
+          style={{
+            transform: isVisible ? "translateY(0)" : "translateY(90%)",
+          }}
+        >
+          <h2>What's next:</h2>
+          <ul className={styles.skillsList}>
+            <li>Next</li>
+            <li>Node.js</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
